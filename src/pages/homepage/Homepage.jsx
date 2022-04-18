baeab1a7e2a304e2fc692b69d542087d26a90117
Мой профиль <91414139+ilyasrv@users.noreply.github.com>
2022-04-18 14:09:39 +0300

    Update Homepage.jsx

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router";
-import Header from "../../components/header/header";
+import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 
 import Footer from './../../components/Footer/footer';
